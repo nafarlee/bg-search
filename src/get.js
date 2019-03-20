62d0d401ef558e9c64cc909cb9f68dc3789f8c4d
Nicholas Farley <nicholasafarley@gmail.com>
2019-03-20 19:53:52 -0400

    Add promise-based https get function

---
+++ src/get.js
@@ -0,0 +1,18 @@
+const https = require('https');
+
+function get(url) {
+  return new Promise((resolve, reject) => {
+    https.get(url, (res) => {
+      if (res.statusCode < 200 || res.statusCode >= 300) {
+        reject(res.statusCode);
+        return;
+      }
+      const body = [];
+      res.on('data', chunk => body.push(chunk));
+      res.on('end', () => resolve(Buffer.concat(body).toString()));
+    })
+      .on('error', err => reject(err));
+  });
+}
+
+module.exports = get;
