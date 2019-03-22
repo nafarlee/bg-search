0fdbe2d3d8015e633898ec41d54daee7b238ef88
Nicholas Farley <nicholasafarley@gmail.com>
2019-03-22 01:22:47 -0400

    Improve index testing script

--- index.js
+++ index.js
@@ -5,15 +5,18 @@ const parseString = promisify(require('xml2js').parseString);
 const get = require('./src/get');
 const marshall = require('./src/marshall');
 
+function randomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
 const baseURL = 'https://api.geekdo.com/xmlapi2/things';
+const id = [...Array(500).keys()].map(() => randomInt(270000));
 const start = process.hrtime();
-get(`${baseURL}?id=13&type=boardgame`)
+get(`${baseURL}?id=${id}&stats=1&type=boardgame,boardgameexpansion`)
   .then(parseString)
   .then((body) => {
     if (!body.items.item) {
       return;
     }
-    const native = body.items.item.map(marshall);
-    console.log(JSON.stringify(native, undefined, 2));
-    console.error(process.hrtime(start));
+    console.log(body.items.item.map(marshall));
   });
