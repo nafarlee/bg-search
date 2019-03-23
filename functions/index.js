7f02d04145c02069a03cbe201ced9036fef96a9c
Nicholas Farley <nicholasafarley@gmail.com>
2019-03-23 11:43:13 -0400

    Fix missing property access for pubsub fn

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 
 exports.pull = functions
+  .pubsub
   .topic('pull')
   .onPublish((message) => {
     console.log(`Firebase Functions just got a message: ${message}`);
