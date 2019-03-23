b89fafc60be34cc62b7b635d69bc39828c70715f
Nicholas Farley <nicholasafarley@gmail.com>
2019-03-23 11:41:06 -0400

    Specify pubsub based cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,7 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.pull = functions
+  .topic('pull')
+  .onPublish((message) => {
+    console.log(`Firebase Functions just got a message: ${message}`);
+  });
