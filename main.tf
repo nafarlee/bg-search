a01f9828b1aad33098cd9303f275a828a740e1ee
Nicholas Farley <nicholasafarley@gmail.com>
2019-03-24 23:15:51 -0400

    Add scheduler for pull topic to terraform file

--- main.tf
+++ main.tf
@@ -7,3 +7,14 @@ provider "google" {
 resource "google_pubsub_topic" "default" {
   name = "pull"
 }
+
+resource "google_cloud_scheduler_job" "default" {
+  name        = "pull-job"
+  description = "Emit 'pull' every 15 minutes"
+  schedule    = "*/15 * * * *"
+
+  pubsub_target {
+    topic_name = "${google_pubsub_topic.default.id}"
+    data       = "${base64encode("_")}"
+  }
+}
