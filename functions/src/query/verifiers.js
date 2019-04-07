e9b240777bdecf4bad9b8edefd0e4c4c42c5f06b
Nicholas Farley <nicholasafarley@gmail.com>
2019-04-06 23:56:08 -0400

    Add generic case-insensitive string includes fn

--- functions/src/query/verifiers.js
+++ functions/src/query/verifiers.js
@@ -1,3 +1,9 @@
+function includes(str, substr) {
+  return str
+    .toLowercase()
+    .includes(substr.toLowerCase());
+}
+
 function NAME(term, game) {
   return game['primary-name']
     .toLowerCase()
