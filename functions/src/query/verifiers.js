225fa6fd61d4a15eb28c5ce81544fb7dfc7a7ca6
Nicholas Farley <nicholasafarley@gmail.com>
2019-04-07 00:15:05 -0400

    Add any helper function

--- functions/src/query/verifiers.js
+++ functions/src/query/verifiers.js
@@ -4,6 +4,13 @@ function includes(str, substr) {
     .includes(substr.toLowerCase());
 }
 
+function any(xs, pred) {
+  for (const x of xs) {
+    if (pred(x)) return true;
+  }
+  return false;
+}
+
 function NAME(term, game) {
   return includes(game['primary-name'], term.value);
 }
