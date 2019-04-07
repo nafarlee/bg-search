748c709f5f302695803fe6664b3521718cc250eb
Nicholas Farley <nicholasafarley@gmail.com>
2019-04-07 00:45:21 -0400

    Use lodash#some instead of custom any fn

--- functions/src/query/verifiers.js
+++ functions/src/query/verifiers.js
@@ -2,23 +2,16 @@ const _ = require('lodash');
 
 const includes = (str, substr) => _.includes(_.toLower(str), _.toLower(substr));
 
-function any(xs, pred) {
-  for (const x of xs) {
-    if (pred(x)) return true;
-  }
-  return false;
-}
-
 function NAME(term, game) {
   return includes(game['primary-name'], term.value);
 }
 
 function ARTIST(term, game) {
-  return any(game.artists, a => includes(a, term.value));
+  return _.some(game.artists, a => includes(a, term.value));
 }
 
 function CATEGORY(term, game) {
-  return any(game.categories, c => includes(c, term.value));
+  return _.some(game.categories, c => includes(c, term.value));
 }
 
 function DESCRIPTION(term, game) {
@@ -26,19 +19,19 @@ function DESCRIPTION(term, game) {
 }
 
 function FAMILY(term, game) {
-  return any(game.families, f => includes(f, term.value));
+  return _.some(game.families, f => includes(f, term.value));
 }
 
 function MECHANIC(term, game) {
-  return any(game.mechanics, m => includes(m, term.value));
+  return _.some(game.mechanics, m => includes(m, term.value));
 }
 
 function PUBLISHER(term, game) {
-  return any(game.publishers, p => includes(p, term.value));
+  return _.some(game.publishers, p => includes(p, term.value));
 }
 
 function DESIGNER(term, game) {
-  return any(game.designers, d => includes(d, term.value));
+  return _.some(game.designers, d => includes(d, term.value));
 }
 
 const operators = {
