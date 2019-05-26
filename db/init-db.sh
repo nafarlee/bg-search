ecdd200c5f95cef418c913abd7e0763f7fa66b26
Nicholas Farley <nicholasafarley@gmail.com>
2019-05-26 13:56:29 -0400

    Update init-db.sh script to not hardcode paths

--- db/init-db.sh
+++ db/init-db.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 docker run --network host \
            -it \
-           -v /home/nafarlee/Projects/bg-search:/_ \
+           -v "$PWD":/_ \
            postgres:11.3 \
            sh -c "psql -h localhost -U postgres -f _/db.pgsql && psql -h localhost -U postgres -f _/data.pgsql"
