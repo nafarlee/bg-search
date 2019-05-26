33d34877ec121cfac976e4a38e8cedd928eebb62
Nicholas Farley <nicholasafarley@gmail.com>
2019-05-26 14:18:21 -0400

    Add function for translating ARTIST requests to SQL

--- db/transpile.js
+++ db/transpile.js
@@ -4,3 +4,12 @@ module.exports.name = (value, negate = false) => ({
   text: `SELECT ${FIELDS} FROM games WHERE primary_name ${negate ? '!' : ''}~~* $1`,
   values: [`%${value}%`],
 });
+
+module.exports.ARTIST = (value, negate = false) => ({
+  text: `SELECT DISTINCT ${FIELDS}
+         FROM games g, games_artists ga, artists a
+         WHERE g.id = ga.game_id
+           AND ga.artist_id = a.id
+           AND artist ${negate ? '!' : ''}~~* $1`,
+  values: [`%${value}%`],
+});
