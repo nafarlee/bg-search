ed16c1bf73bc5df703d43cdeb49edbea15ab07a7
Nicholas Farley <nicholasafarley@gmail.com>
2019-05-28 21:37:24 -0400

    Redefine NAME and DESCRIPTION via junction fn

--- db/transpile.js
+++ db/transpile.js
@@ -7,23 +7,20 @@ const simple = field => (value, negate = false) => ({
   values: [`%${value}%`],
 });
 
+const junction = ({
+  target,
+  field,
+}) => (value, negate = false) => ({
+  text: `SELECT DISTINCT ${FIELDS}
+         FROM games a, games_${target} ab, ${target} b
+         WHERE a.id = ab.game_id
+           AND ab.${field}_id = b.id
+           AND ${field} ${negate ? '!' : ''}~~* $1`,
+  values: [`%${value}%`],
+});
+
 module.exports.NAME = simple('primary_name');
 module.exports.DESCRIPTION = simple('description');
 
-module.exports.ARTIST = (value, negate = false) => ({
-  text: `SELECT DISTINCT ${FIELDS}
-         FROM games g, games_artists ga, artists a
-         WHERE g.id = ga.game_id
-           AND ga.artist_id = a.id
-           AND artist ${negate ? '!' : ''}~~* $1`,
-  values: [`%${value}%`],
-});
-
-module.exports.CATEGORY = (value, negate = false) => ({
-  text: `SELECT DISTINCT ${FIELDS}
-         FROM games g, games_categories gc, categories c
-         WHERE g.id = gc.game_id
-           AND gc.category_id = c.id
-           AND category ${negate ? '!' : ''}~~* $1`,
-  values: [`%${value}%`],
-});
+module.exports.ARTIST = junction({ target: 'artists', field: 'artist' });
+module.exports.CATEGORY = junction({ target: 'categories', field: 'category' });
