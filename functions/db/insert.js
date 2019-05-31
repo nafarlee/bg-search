34242e43139bc4a8f2dd989305197e2032a360aa
Nicholas Farley <nicholasafarley@gmail.com>
2019-05-30 22:07:02 -0400

    Add first try at insert query generator

---
+++ functions/db/insert.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+function insert(game) {
+  const fields = {
+    id: game.id,
+    image: game.image,
+    thumbnail: game.thumbnail,
+    last_updated: 'DEFAULT',
+    average_rating: game['average-rating'],
+    average_weight: game['average-weight'],
+    bayes_rating: game['bayes-rating'],
+    description: game.description,
+    maximum_players: game['maximum-players'],
+    maximum_playtime: game['maximum-playtime'],
+    minimum_age: game['minimum-age'],
+    minimum_players: game['minimum-players'],
+    minimum_playtime: game['minimum-playtime'],
+    primary_name: game['primary-name'],
+    rating_deviation: game['rating-deviation'],
+    rating_votes: game['rating-votes'],
+    weight_votes: game['weight-votes'],
+    year: game.year,
+  };
+
+  const keys = Object.keys(fields);
+  const positions = keys.map((_, i) => `$${i + 1}`).join(', ');
+
+  return [
+    `INSERT INTO games (${keys.join(', ')})
+     VALUES ( ${positions} );`,
+    keys.map(k => fields[k]),
+  ];
+}
+
+module.exports = insert;
