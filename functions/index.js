8fe0e9c0b27de40f747b3dbf7005c1c890e1b34d
Nicholas Farley <nicholasafarley@gmail.com>
2019-06-09 14:38:28 -0400

    Modify pull topic to call pull function

--- functions/index.js
+++ functions/index.js
@@ -1,52 +1,18 @@
-const { promisify } = require('util');
 
-const parseString = promisify(require('xml2js').parseString);
-const _ = require('lodash');
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
-const get = require('./src/get');
-const marshall = require('./src/marshall');
 const language = require('./src/language');
 const verify = require('./src/query');
 const views = require('./src/views');
-
-const baseURL = 'https://api.geekdo.com/xmlapi2/things';
+const pull = require('./src/endpoints/pull');
 
 admin.initializeApp();
 
 exports.pull = functions
   .pubsub
   .topic('pull')
-  .onPublish(async () => {
-    const db = admin.firestore();
-    const batch = db.batch();
-    const doc = await db.collection('_').doc('_').get();
-
-    if (!doc.exists) throw new Error('Metadata document not found!');
-    const index = doc.get('index');
-    const newIndex = index + 500;
-    const IDs = _.range(index, newIndex).join(',');
-    batch.update(db.collection('_').doc('_'), { index: newIndex });
-    const xml = await get(`${baseURL}?stats=1&type=boardgame,boardgameexpansion&id=${IDs}`);
-
-    const body = await parseString(xml);
-
-    if (!body.items.item) {
-      return db.collection('_').doc('_').update({ index: 1 });
-    }
-
-    body.items.item.forEach((item) => {
-      try {
-        const native = marshall(item);
-        batch.set(db.collection('games').doc(`${native.id}`), native);
-      } catch (err) {
-        console.error('ERROR WITH ID: ', item.$.id);
-        throw err;
-      }
-    });
-    return batch.commit();
-  });
+  .onPublish(pull);
 
 exports.search = functions
   .runWith({ timeoutSeconds: 540 })
