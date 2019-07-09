2724d4d0cd3f3d71254532fb76dd279c883b4607
Nicholas Farley <nicholasafarley@gmail.com>
2019-07-09 01:58:49 -0400

    Create basic reduction algorithm for SQL folding

--- functions/src/transpile/index.js
+++ functions/src/transpile/index.js
@@ -1,27 +1,17 @@
 const transpile = require('./lib');
 const language = require('../language');
 
-function toArgs(p) {
-  switch (p.type) {
-    case 'META': return [p.negate];
-    case 'DECLARATIVE': return [p.value, p.negate];
-    case 'RELATIONAL': return [p.operator, p.value, p.negate];
-    default: throw new Error('Unkown predicate type');
-  }
-}
-
 function query(s) {
-  const predicates = language.tryParse(s);
-  const result = predicates.map((p) => {
-    const fn = transpile[p.tag];
-    return fn(...toArgs(p));
-  });
-
-  const appension = result.map(r => r.text).join('\nINTERSECT\n');
-  const pre = `SELECT DISTINCT primary_name, year
-               FROM games`;
-  const post = 'ORDER BY year';
-  return `${pre} INTERSECT (${appension}) ${post};`;
+  return language
+    .tryParse(s)
+    .reduce((acc, cur) => {
+      if (cur.type === 'OR') return acc;
+      const { text, values } = transpile[cur.tag](cur);
+      return {
+        text: (acc.text.length === 0) ? text : `${acc.text} INTERSECT ${text}`,
+        values: acc.values.concat(values || []),
+      };
+    }, { text: '', values: [] });
 }
 
 module.exports = query;
