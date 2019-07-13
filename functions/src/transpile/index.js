52cd3ca16ca47b2664f457c1ea439278201517b5
Nicholas Farley <nicholasafarley@gmail.com>
2019-07-13 16:29:06 -0400

    Finish query transpiler

--- functions/src/transpile/index.js
+++ functions/src/transpile/index.js
@@ -1,17 +1,35 @@
 const transpile = require('./lib');
 const language = require('../language');
 
-function query(s) {
-  return language
-    .tryParse(s)
-    .reduce((acc, cur) => {
-      if (cur.type === 'OR') return acc;
-      const { text, values } = transpile[cur.tag](cur);
+function toSQL(predicates, intersect = true) {
+  const joiningTerm = intersect ? 'INTERSECT' : 'UNION';
+  return predicates.reduce((acc, cur) => {
+    if (cur.type === 'OR') {
+      const { text, values } = toSQL(cur.terms, false);
       return {
-        text: (acc.text.length === 0) ? text : `${acc.text} INTERSECT ${text}`,
+        text: `${acc.text} INTERSECT (${text})`,
         values: acc.values.concat(values || []),
       };
-    }, { text: '', values: [] });
+    }
+    const { text, values } = transpile[cur.tag](cur);
+    return {
+      text: (acc.text.length === 0)
+        ? text
+        : `${acc.text} ${joiningTerm} ${text}`,
+      values: acc.values.concat(values || []),
+    };
+  }, { text: '', values: [] });
+}
+
+function query(s) {
+  const predicates = language.tryParse(s);
+  const { text, values } = toSQL(predicates);
+
+  const replacer = (x => () => `$${x++}`)(1);
+  return {
+    text: text.replace(/\{\{\}\}/g, replacer),
+    values,
+  };
 }
 
 module.exports = query;
