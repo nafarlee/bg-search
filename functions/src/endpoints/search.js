8c6ec5161349969d8aa72317dfc8cc0cbb2ae36f
Nicholas Farley <nicholasafarley@gmail.com>
2019-07-13 18:06:19 -0400

    Modify search endpoint to work with postgres

--- functions/src/endpoints/search.js
+++ functions/src/endpoints/search.js
@@ -1,6 +1,6 @@
-const language = require('../language');
-const verify = require('../query');
-const views = require('../views');
+const { Client } = require('pg');
+
+const transpile = require('../transpile');
 
 module.exports = async function search(req, res) {
   res.set('Cache-Control', `public, max-age=${60 * 60 * 24 * 7}`);
@@ -8,31 +8,16 @@ module.exports = async function search(req, res) {
   const order = req.query.order || 'bayes-rating';
   const direction = req.query.direction || 'desc';
   console.log({ query, order, direction });
-  const predicates = language.tryParse(query);
-
-  const db = admin.firestore();
-  const results = [];
-  let col = db.collection('games').orderBy(order, direction);
-  if (req.query.checkpoint) {
-    const snapshot = await db
-      .collection('games')
-      .doc(req.query.checkpoint)
-      .get();
-    col = col.startAfter(snapshot);
+  const sql = transpile(query);
+  const client = new Client({
+    user: 'postgres',
+    database: 'postgres',
+  });
+  try {
+    await client.connect();
+    const { rows } = await client.query(sql);
+    console.log(rows);
+  } finally {
+    client.end();
   }
-  return col
-    .stream()
-    .on('data', (doc) => {
-      const data = doc.data();
-      try {
-        if (verify(predicates, data)) results.push(data);
-      } catch (e) {
-        console.error({ query, game: data });
-        throw e;
-      }
-      if (results.length >= 10) {
-        res.status(200).send(views.search({ req, fnName: 'search', games: results }));
-      }
-    })
-    .on('end', () => res.status(200).send(views.search({ req, games: results })));
 };
