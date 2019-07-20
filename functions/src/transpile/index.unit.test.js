086e04bcddef1904ee175481e0bdb73c8f1db76f
Nicholas Farley <nicholasafarley@gmail.com>
2019-07-20 17:50:42 -0400

    Change transpilation unit test to do sql string compare

--- functions/src/transpile/index.unit.test.js
+++ functions/src/transpile/index.unit.test.js
@@ -1,27 +1,68 @@
-const { Client } = require('pg');
-
 const transpile = require('./index');
 
-let client;
-
-beforeAll(() => {
-  client = new Client({
-    user: 'postgres',
-    database: 'postgres',
-  });
-  return client.connect();
-});
-
-afterAll(() => {
-  client.end();
+expect.extend({
+  toMatchIgnoringWhitespace(received, other) {
+    const regex = /\s+/g;
+    return {
+      pass: received.replace(regex, '') === other.replace(regex, ''),
+    };
+  },
 });
 
 test("a cooperative dice game that isn't a collection with at least 500 ratings", () => {
   const input = '-is:expansion -is:collection rating-votes>=500 mechanic:coop mechanic:dice';
-  return client.query(transpile(input, 'bayes_rating', 'DESC', 0));
+  const { text, values } = transpile(input, 'bayes_rating', 'DESC', 0);
+  expect(text).toMatchIgnoringWhitespace(`
+    SELECT primary_name, rating_votes, average_rating, bayes_rating, rating_deviation, average_weight, weight_votes, year, minimum_age, minimum_players, maximum_players, minimum_playtime, maximum_playtime, description
+              FROM games
+              INTERSECT
+              SELECT primary_name, rating_votes, average_rating, bayes_rating, rating_deviation, average_weight, weight_votes, year, minimum_age, minimum_players, maximum_players, minimum_playtime, maximum_playtime, description
+               FROM games
+               LEFT JOIN expansions
+                 ON id = expansion
+               WHERE base IS  NULL INTERSECT SELECT primary_name, rating_votes, average_rating, bayes_rating, rating_deviation, average_weight, weight_votes, year, minimum_age, minimum_players, maximum_players, minimum_playtime, maximum_playtime, description
+               FROM games
+               LEFT JOIN collections
+                 ON id = collection
+               WHERE item IS  NULL INTERSECT SELECT primary_name, rating_votes, average_rating, bayes_rating, rating_deviation, average_weight, weight_votes, year, minimum_age, minimum_players, maximum_players, minimum_playtime, maximum_playtime, description
+             FROM games
+             WHERE  rating_votes >= $1  INTERSECT SELECT DISTINCT primary_name, rating_votes, average_rating, bayes_rating, rating_deviation, average_weight, weight_votes, year, minimum_age, minimum_players, maximum_players, minimum_playtime, maximum_playtime, description
+             FROM games a, games_mechanics ab, mechanics b
+             WHERE a.id = ab.game_id
+               AND ab.mechanic_id = b.id
+               AND mechanic ~~* $2 INTERSECT SELECT DISTINCT primary_name, rating_votes, average_rating, bayes_rating, rating_deviation, average_weight, weight_votes, year, minimum_age, minimum_players, maximum_players, minimum_playtime, maximum_playtime, description
+             FROM games a, games_mechanics ab, mechanics b
+             WHERE a.id = ab.game_id
+               AND ab.mechanic_id = b.id
+               AND mechanic ~~* $3
+              ORDER BY bayes_rating DESC
+              LIMIT 25 OFFSET $4
+  `);
+  expect(values).toEqual(['500', '%coop%', '%dice%', 0]);
 });
 
 test('a worker placement game that is best with 2 or 3 players', () => {
   const input = 'mechanic:worker best-players=2 or best-players=3';
-  return client.query(transpile(input, 'bayes_rating', 'DESC', 0));
+  const { text, values } = transpile(input, 'bayes_rating', 'DESC', 0);
+  expect(text).toMatchIgnoringWhitespace(`
+    SELECT primary_name, rating_votes, average_rating, bayes_rating, rating_deviation, average_weight, weight_votes, year, minimum_age, minimum_players, maximum_players, minimum_playtime, maximum_playtime, description
+              FROM games
+              INTERSECT
+              (SELECT DISTINCT primary_name, rating_votes, average_rating, bayes_rating, rating_deviation, average_weight, weight_votes, year, minimum_age, minimum_players, maximum_players, minimum_playtime, maximum_playtime, description
+             FROM games a, games_mechanics ab, mechanics b
+             WHERE a.id = ab.game_id
+               AND ab.mechanic_id = b.id
+               AND mechanic ~~* $1 INTERSECT SELECT primary_name, rating_votes, average_rating, bayes_rating, rating_deviation, average_weight, weight_votes, year, minimum_age, minimum_players, maximum_players, minimum_playtime, maximum_playtime, description
+               FROM games g, player_recommendations pr
+               WHERE g.id = pr.id
+                 AND players && $2::int4range
+                 AND  best > (recommended + not_recommended) UNION SELECT primary_name, rating_votes, average_rating, bayes_rating, rating_deviation, average_weight, weight_votes, year, minimum_age, minimum_players, maximum_players, minimum_playtime, maximum_playtime, description
+               FROM games g, player_recommendations pr
+               WHERE g.id = pr.id
+                 AND players && $3::int4range
+                 AND  best > (recommended + not_recommended))
+              ORDER BY bayes_rating DESC
+              LIMIT 25 OFFSET $4
+  `);
+  expect(values).toEqual(['%worker%', '[2,2]', '[3,3]', 0]);
 });
