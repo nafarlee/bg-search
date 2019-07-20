27e8f548b494b7a5ab42b98cbb06fb437cee3c13
Nicholas Farley <nicholasafarley@gmail.com>
2019-07-20 17:10:02 -0400

    Add offset parameter to invocations to transpile

--- functions/src/transpile/index.unit.test.js
+++ functions/src/transpile/index.unit.test.js
@@ -18,10 +18,10 @@ afterAll(() => {
 
 test("a cooperative dice game that isn't a collection with at least 500 ratings", () => {
   const input = '-is:expansion -is:collection rating-votes>=500 mechanic:coop mechanic:dice';
-  return client.query(transpile(input, 'bayes_rating', 'DESC'));
+  return client.query(transpile(input, 'bayes_rating', 'DESC', 0));
 });
 
 test('a worker placement game that is best with 2 or 3 players', () => {
   const input = 'mechanic:worker best-players=2 or best-players=3';
-  return client.query(transpile(input, 'bayes_rating', 'DESC'));
+  return client.query(transpile(input, 'bayes_rating', 'DESC', 0));
 });
