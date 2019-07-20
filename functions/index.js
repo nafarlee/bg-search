6133bb601d005f916884594d102411f53d6acc2c
Nicholas Farley <nicholasafarley@gmail.com>
2019-07-20 19:11:12 -0400

    Remove firebase-admin usage

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,7 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin');
 
 const pull = require('./src/endpoints/pull');
 const search = require('./src/endpoints/search');
-
-admin.initializeApp();
 
 exports.pull = functions
   .pubsub
