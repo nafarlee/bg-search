8c8b9a3459d7aa2b0149fa2b119cf18f5b7332cd
Nicholas Farley <nicholasafarley@gmail.com>
2019-08-04 01:48:40 -0400

    Revert "Update test for view changes"
    This reverts commit cac957596496d7c86c1d032b16b8a85a8db7fa08.

--- functions/src/views.unit.test.js
+++ functions/src/views.unit.test.js
@@ -11,7 +11,6 @@ test('single game', () => {
       },
     },
     games: [{
-      thumbnail: '_.png',
       primary_name: 'Senet',
       id: 2399,
       year: -3500,
@@ -20,12 +19,7 @@ test('single game', () => {
   });
   const expected = `
     <!DOCTYPE html>
-    
-      <section>
-        <img src="_.png" />
-        <h2><a href="https://boardgamegeek.com/boardgame/2399">Senet (-3500)</a></h2>
-      </section>
-    
+    <h2><a href="https://boardgamegeek.com/boardgame/2399">Senet (-3500)</a></h2>
     <br>
     <p><a href="https://us-central1-bg-search.cloudfunctions.net?query=&order=bayes_rating&direction=DESC&offset=1">Next</a></p>
   `;
