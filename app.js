9eec594dcabf9a7f03d8df5082743b5ff31f65fc
Nicholas Farley <nicholasafarley@gmail.com>
2020-07-11 23:30:47 -0400

    Add /pubsub/pull-plays endpoints

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const pull = require('./src/endpoints/pull');
 const search = require('./src/endpoints/search');
+const pullPlays = require('./src/endpoints/pull-plays');
 
 const app = express();
 app.set('view engine', 'pug');
 
 app.get('/search', search);
 app.post('/pubsub/pull', pull);
+app.post('/pubsub/pull-plays', pullPlays);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {});
