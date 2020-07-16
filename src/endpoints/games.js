d25c8742596f23ac604dfc7d83fc08e66b2b110f
Nicholas Farley <nicholasafarley@gmail.com>
2020-07-16 00:37:42 -0400

    Show all DB fields automatically

--- src/endpoints/games.js
+++ src/endpoints/games.js
@@ -6,7 +6,7 @@ module.exports = async function search(req, res) {
   res.set('Cache-Control', `public, max-age=${60 * 60 * 24 * 7}`);
   const { id } = req.params;
   const client = new Client(credentials);
-  const sql = `SELECT primary_name
+  const sql = `SELECT *
                FROM games
                WHERE id = $1`;
   try {
