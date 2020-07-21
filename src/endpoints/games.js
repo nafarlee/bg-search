76eeb7124280b70672d7b442235327ce3e651938
Nicholas Farley <nicholasafarley@gmail.com>
2020-07-20 22:04:51 -0400

    Explicitly spell out fields to select on

--- src/endpoints/games.js
+++ src/endpoints/games.js
@@ -6,9 +6,26 @@ module.exports = async function search(req, res) {
   res.set('Cache-Control', `public, max-age=${60 * 60 * 24 * 7}`);
   const { id } = req.params;
   const client = new Client(credentials);
-  const sql = `SELECT *
+  const sql = `SELECT
+                 id,
+                 image,
+                 average_rating,
+                 average_weight,
+                 bayes_rating,
+                 description,
+                 maximum_players,
+                 maximum_playtime,
+                 minimum_age,
+                 minimum_players,
+                 minimum_playtime,
+                 primary_name,
+                 rating_deviation,
+                 rating_votes,
+                 weight_votes,
+                 year
                FROM games
-               WHERE id = $1`;
+               WHERE id = $1
+               GROUP BY id`;
   try {
     await client.connect();
     const { rows: games } = await client.query(sql, [id]);
