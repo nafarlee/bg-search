de812d44fb423bd176e74f14209af1bf71c02f35
Nicholas Farley <nicholasafarley@gmail.com>
2021-01-02 02:00:05 -0500

    Add "blocking" higher-order throttle function

---
+++ src/throttle.js
@@ -0,0 +1,15 @@
+module.exports = function throttle(fn, wait) {
+  let last = null;
+  return async (...args) => {
+    if (last === null) {
+      last = Date.now();
+      return fn(...args);
+    }
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        last = Date.now();
+        return resolve((fn(...args)));
+      }, last + wait - Date.now());
+    });
+  };
+};
