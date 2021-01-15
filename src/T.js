49ca62e81a19ca20c3745b8c419ccd1f1b248961
Nicholas Farley <nicholasafarley@gmail.com>
2021-01-14 22:19:24 -0500

    Add function for attempting risky operations

---
+++ src/T.js
@@ -0,0 +1,17 @@
+module.exports = (risk) => {
+  if (risk instanceof Function) {
+    try {
+      return [null, risk()];
+    } catch (error) {
+      return [error, null];
+    }
+  }
+
+  if (risk instanceof Promise) {
+    return risk
+      .then((x) => [null, x])
+      .catch((error) => [error, null]);
+  }
+
+  throw new Error('Unsupported type passed to "try"');
+};
