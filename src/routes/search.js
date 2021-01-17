4f3445375f309777a8a377f0ac9682130ed621f5
Nicholas Farley <nicholasafarley@gmail.com>
2021-01-16 23:43:32 -0500

    Show a much more "kind" error on search syntax issues

--- src/routes/search.js
+++ src/routes/search.js
@@ -1,6 +1,7 @@
 const { format } = require('url');
 
 const { Client } = require('pg');
+const _ = require('lodash');
 
 const transpile = require('../transpile');
 const credentials = require('../../db-credentials');
@@ -22,7 +23,16 @@ module.exports = async function search(req, res) {
   });
 
   const [transpilationError, sql] = T(() => transpile(query, order, direction, offset));
-  if (transpilationError) return res.status(400).send(transpilationError);
+  if (transpilationError) {
+    const code = 422;
+    const message = 'Your search has an error! Hopefully the hint below will help find it.';
+    const padding = ' '.repeat(_.get(transpilationError, 'result.index.offset'));
+    const annotation = `${padding}^ The error should start here`;
+    const block = `${query}\n${annotation}`;
+    return res
+      .status(code)
+      .render('error', { code, block, message });
+  }
 
   const client = new Client(credentials);
   const [connectionError] = await T(client.connect());
