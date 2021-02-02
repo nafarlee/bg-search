b7aa6d7cf6cc4af66da8b1de4bb4a8499c0b6642
Nicholas Farley <nicholasafarley@gmail.com>
2021-02-01 21:27:01 -0500

    Modernize src/routes/pull.js

--- src/routes/pull.js
+++ src/routes/pull.js
@@ -1,17 +1,18 @@
-const { promisify } = require('util');
+import { promisify } from 'util';
 
-const parseString = promisify(require('xml2js').parseString);
-const { Client } = require('pg');
-const { range } = require('lodash');
+import { Client } from 'pg';
+import { range } from 'lodash';
+import xml2js from 'xml2js';
 
-const get = require('../get');
-const marshall = require('../marshall');
-const { insert } = require('../db/insert');
-const credentials = require('../../db-credentials');
+import get from '../get';
+import marshall from '../marshall';
+import { insert } from '../db/insert';
+import credentials from '../../db-credentials.json';
 
+const parseString = promisify(xml2js.parseString);
 const baseURL = 'https://api.geekdo.com/xmlapi2/things';
 
-async function pull(req, res) {
+export default async function pull(req, res) {
   const client = new Client(credentials);
 
   await client.connect();
@@ -45,5 +46,3 @@ async function pull(req, res) {
     await client.end();
   }
 }
-
-module.exports = pull;
