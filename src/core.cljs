b04b8e388f8254a6d2b1ff81ba68f543fdf053f3
Nicholas Farley <nicholasafarley@gmail.com>
2021-02-02 01:58:25 -0500

    Add replacement for app.js

--- src/core.cljs
+++ src/core.cljs
@@ -1,8 +1,24 @@
 (ns core
   (:require
-    ["/get" :default http-get]))
+    [shadow.resource :as rc]
+    ["express" :as express]
+    ["/routes/pull" :default pull]
+    ["/routes/search" :default search]
+    ["/routes/pull-plays" :default pull-plays]
+    ["/routes/games" :default games]
+    ["/views/locals" :as locals]))
 
-(defn main [& args]
-  (-> "https://jsonplaceholder.typicode.com/todos/1"
-      http-get
-      (.then js/console.log)))
+(defonce ^:export app (express))
+
+(defonce credentials (rc/inline "/db-credentials.json"))
+
+(defn main []
+  (set! (.-locals app) (merge (.-locals app) locals))
+  (doto app
+        (.set "view engine" "pug")
+        (.use (.static express "public"))
+        (.get "/search" (search credentials))
+        (.get "/pubsub/pull" (pull credentials))
+        (.get "/pubsub/pull-plays" (pull-plays credentials))
+        (.get "/games/:id" (games credentials))
+        (.listen 8080 #(prn "Listening on 8080..."))))
