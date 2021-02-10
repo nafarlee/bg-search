17e9aaab6f8a848c96fa7b833d6254cb990f6ad2
Nicholas Farley <nicholasafarley@gmail.com>
2021-02-10 00:31:54 -0500

    Add error/transpile fn

---
+++ src/error.cljs
@@ -0,0 +1,15 @@
+(ns error)
+
+(defn transpile [error res query]
+  (let [code       422
+        message    (str "Your search has an error! Hopefully the hint below"
+                        " will help you find it.")
+        offset     (or (.. error -result -index -offset) 0)
+        padding    (apply str (repeat offset " "))
+        annotation (str padding "^ This is the first 'bad' character")
+        block      (str query "\n" annotation)]
+    (-> res
+        (.status code)
+        (.render "error" #js{:code code
+                             :message message
+                             :block block}))))
