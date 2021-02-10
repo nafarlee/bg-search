83c1ae460e3686c7790f14c2bcfacedaff70e0d3
Nicholas Farley <nicholasafarley@gmail.com>
2021-02-10 00:34:42 -0500

    Expand upon routes/search fn

--- src/routes.cljs
+++ src/routes.cljs
@@ -1,10 +1,17 @@
-(ns routes)
+(ns routes
+  (:require
+   [error :as err]
+   [result :as rs]
+   ["/transpile/index" :default transpile]))
 
 (defn search [req res]
-  (.set res
-        "Cache-Control"
-        (format "public, max-age=%s" (* 60 60 24 7)))
+  (.set res "Cache-Control" (str "public, max-age=" (* 60 60 24 7)))
   (let [query     (or (.. req -query -query) "")
         order     (or (.. req -query -order) "bayes_rating")
         direction (or (.. req -query -direction) "DESC")
-        offset    (or (-> req (.. -query -offset) (js/parseInt 10)) 0)]))
+        offset    (-> (or (.. req -query -offset) 0) (js/parseInt 10))]
+    (prn {:query query :order order :direction direction :offset offset})
+    (let [sql-result (rs/attempt transpile query order direction offset)]
+      (if (rs/error? sql-result)
+        (err/transpile (rs/unwrap sql-result) res query)
+        (.send res "Success")))))
