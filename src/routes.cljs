961b11e5b78246718e08d0833f5aa9f3eceae234
Nicholas Farley <nicholasafarley@gmail.com>
2021-02-10 01:50:35 -0500

    Finalize direct port of src/routes/search.js

--- src/routes.cljs
+++ src/routes.cljs
@@ -1,8 +1,13 @@
 (ns routes
   (:require
+   ["url" :as url]
+   ["pg" :refer [Client]]
+   ["/transpile/index" :default transpile]
+   [cljs.core.async :refer [go]]
+   [cljs.core.async.interop :refer-macros [<p!]]
+   [static :refer [credentials]]
    [error :as err]
-   [result :as rs]
-   ["/transpile/index" :default transpile]))
+   [result :as rs]))
 
 (defn search [req res]
   (.set res "Cache-Control" (str "public, max-age=" (* 60 60 24 7)))
@@ -14,4 +19,37 @@
     (let [sql-result (rs/attempt transpile query order direction offset)]
       (if (rs/error? sql-result)
         (err/transpile (rs/unwrap sql-result) res query)
-        (.send res "Success")))))
+        (go
+          (let [client         (Client. credentials)
+                connect-result (<p! (rs/from-promise (.connect client)))]
+            (if (rs/error? connect-result)
+              (do
+                (.end client)
+                (-> res
+                    (.status 500)
+                    (.send (rs/unwrap connect-result))))
+              (let [sql          (rs/unwrap sql-result)
+                    query-result (<p! (rs/from-promise (.query client sql)))]
+                (.end client)
+                (if (rs/error? query-result)
+                  (-> res
+                      (.status 500)
+                      (.send (rs/unwrap query-result)))
+                  (let [games          (-> query-result
+                                           rs/unwrap
+                                           .-rows)
+                        next-url-query (-> (.-query req)
+                                           js->clj
+                                           (update-in [:offset] + (count games))
+                                           clj->js)
+                        next-url       (-> {:protocol (.-protocol req)
+                                            :host (.get req "host")
+                                            :pathname (.-path req)
+                                            :query next-url-query}
+                                           clj->js
+                                           url/format)]
+                    (.render res "search" (clj->js {:games games
+                                                    :nextURL next-url
+                                                    :query query
+                                                    :order order
+                                                    :direction direction}))))))))))))
