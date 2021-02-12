e8ad43c1e4220dfb3e0f7ea0df4a46066bb5b72e
Nicholas Farley <nicholasafarley@gmail.com>
2021-02-12 01:04:45 -0500

    Changes routes/search to use middleware/with-database

--- src/core.cljs
+++ src/core.cljs
@@ -4,6 +4,7 @@
     ["express" :as express]
     ["/routes/pull" :default pull]
     [routes :as routes]
+    [middleware :refer [with-database]]
     ["/routes/pull-plays" :default pull-plays]
     ["/routes/games" :default games]
     ["/views/locals" :as locals]))
@@ -17,7 +18,7 @@
         (.set "view engine" "pug")
         (.set "views" "src/views")
         (.use (.static express "public"))
-        (.get "/search" routes.search)
+        (.get "/search" (with-database routes.search))
         (.post "/pubsub/pull" (pull credentials))
         (.post "/pubsub/pull-plays" (pull-plays credentials))
         (.get "/games/:id" (games credentials))
--- src/routes.cljs
+++ src/routes.cljs
@@ -1,11 +1,9 @@
 (ns routes
   (:require
    ["url" :as url]
-   ["pg" :refer [Client]]
    ["/transpile/index" :default transpile]
    [cljs.core.async :refer [go]]
    [cljs.core.async.interop :refer-macros [<p!]]
-   [static :refer [credentials]]
    [error :as err]
    [result :as rs]))
 
@@ -14,38 +12,32 @@
   (let [query     (or (.. req -query -query) "")
         order     (or (.. req -query -order) "bayes_rating")
         direction (or (.. req -query -direction) "DESC")
-        offset    (-> (.. req -query -offset) (or 0) (js/parseInt 10))]
+        offset    (-> (.. req -query -offset) (or 0) (js/parseInt 10))
+        database  (.-database req)]
     (prn {:query query :order order :direction direction :offset offset})
     (let [sql-result (rs/attempt transpile query order direction offset)]
       (if (rs/error? sql-result)
         (err/transpile (rs/unwrap sql-result) res query)
         (go
-          (let [client         (Client. credentials)
-                connect-result (<p! (rs/from-promise (.connect client)))]
-            (if (rs/error? connect-result)
-              (do
-                (.end client)
-                (err/generic (rs/unwrap sql-result) res 500))
-              (let [sql          (rs/unwrap sql-result)
-                    query-result (<p! (rs/from-promise (.query client sql)))]
-                (.end client)
-                (if (rs/error? query-result)
-                  (err/generic (rs/unwrap query-result) res 500)
-                  (let [games          (-> query-result
-                                           rs/unwrap
-                                           .-rows)
-                        next-url-query (-> (.-query req)
-                                           js->clj
-                                           (update-in [:offset] + (count games))
-                                           clj->js)
-                        next-url       (-> {:protocol (.-protocol req)
-                                            :host (.get req "host")
-                                            :pathname (.-path req)
-                                            :query next-url-query}
-                                           clj->js
-                                           url/format)]
-                    (.render res "search" (clj->js {:games games
-                                                    :nextURL next-url
-                                                    :query query
-                                                    :order order
-                                                    :direction direction}))))))))))))
+          (let [sql          (rs/unwrap sql-result)
+                query-result (<p! (rs/from-promise (.query database sql)))]
+            (if (rs/error? query-result)
+              (err/generic (rs/unwrap query-result) res 500)
+              (let [games          (-> query-result
+                                       rs/unwrap
+                                       .-rows)
+                    next-url-query (-> (.-query req)
+                                       js->clj
+                                       (update-in [:offset] + (count games))
+                                       clj->js)
+                    next-url       (-> {:protocol (.-protocol req)
+                                        :host (.get req "host")
+                                        :pathname (.-path req)
+                                        :query next-url-query}
+                                       clj->js
+                                       url/format)]
+                (.render res "search" (clj->js {:games games
+                                                :nextURL next-url
+                                                :query query
+                                                :order order
+                                                :direction direction}))))))))))
