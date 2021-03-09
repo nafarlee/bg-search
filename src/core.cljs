aa5d7590c782262f74a3ba638e166a1b6ac15c17
Nicholas Farley <nicholasafarley@gmail.com>
2021-03-09 00:46:43 -0500

    Switch from src/routes/pull-plays.js to routes/pull-plays

--- src/core.cljs
+++ src/core.cljs
@@ -1,10 +1,8 @@
 (ns core
   (:require
-    [static :refer [credentials]]
     ["express" :as express]
     [routes :as routes]
     [middleware :refer [with-database with-header]]
-    ["/routes/pull-plays" :default pull-plays]
     ["/views/locals" :as locals]))
 
 (defonce ^:export app (express))
@@ -21,7 +19,8 @@
                             with-database))
         (.post "/pubsub/pull" (-> routes/pull
                                   with-database))
-        (.post "/pubsub/pull-plays" (pull-plays credentials))
+        (.post "/pubsub/pull-plays" (-> routes/pull-plays
+                                        with-database))
         (.get "/games/:id" (-> routes/games
                                (with-header "Cache-Control" (str "public, max-age=" (* 60 60 24 7)))
                                with-database))
