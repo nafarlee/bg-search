1cd18c4aff8ddb6bce2613c0b47e2acba9fbf00d
Nicholas Farley <nicholasafarley@gmail.com>
2021-04-10 01:27:24 -0400

    Clean up transpile/transpile fn

--- src/transpile.cljs
+++ src/transpile.cljs
@@ -5,26 +5,28 @@
     ["/transpile/lib" :default tl]
     ["/transpile/index" :as t]))
 
-(defn generator [s]
+(defn- create-generator [s]
   (let [remaining (atom s)]
     (fn []
       (let [f (first @remaining)]
         (swap! remaining rest)
         f))))
 
+(def ^:private parameter-templates (map #(str "$" (inc %)) (range)))
+
 (defn transpile [query order direction offset]
-  {:pre [(some (partial = order) (.-FIELDS tl))
+  {:pre [(some (partial = order)
+               (.-FIELDS tl))
          (#{"ASC" "DESC"} direction)]}
-  (let [predicates            (.tryParse lang query)
-        {:strs [text values]} (js->clj (.toSQL t predicates))
-        from                  (if (empty? text)
-                                "games"
-                                (str "(" text ") AS GameSubquery NATURAL INNER JOIN games"))
-        text                  (str "SELECT DISTINCT " (.-CONCATENATED_FIELDS tl) " "
-                                   "FROM " from " "
-                                   "ORDER BY " order " " direction " "
-                                   "LIMIT 25 OFFSET {{}}")
-        values                (conj values offset)
-        yield                 (generator (map #(str "$" (inc %)) (range)))]
-    #js{:text (s/replace text #"\{\{\}\}" yield)
-        :values values}))
+  (let [ast                   (.tryParse lang query)
+        {:strs [text values]} (js->clj (.toSQL t ast))]
+    #js{:values (conj values offset)
+        :text   (as-> text $
+                      (if (empty? $)
+                        "games"
+                        (str "(" $ ") AS GameSubquery NATURAL INNER JOIN games"))
+                      (str "SELECT DISTINCT " (.-CONCATENATED_FIELDS tl)
+                           " FROM " $
+                           " ORDER BY " order " " direction
+                           " LIMIT 25 OFFSET {{}}")
+                      (s/replace $ #"\{\{\}\}" (create-generator parameter-templates)))}))
