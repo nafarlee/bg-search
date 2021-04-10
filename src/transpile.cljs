5a4e885831b56bbc48dbdc22d24f524c366df852
Nicholas Farley <nicholasafarley@gmail.com>
2021-04-10 11:31:46 -0400

    Ensure output of transpile is JS all the way down

--- src/transpile.cljs
+++ src/transpile.cljs
@@ -42,13 +42,13 @@
          (#{"ASC" "DESC"} direction)]}
   (let [ast                   (.tryParse lang query)
         {:strs [text values]} (js->clj (to-sql ast))]
-    #js{:values (conj values offset)
-        :text   (as-> text $
-                      (if (empty? $)
-                        "games"
-                        (str "(" $ ") AS GameSubquery NATURAL INNER JOIN games"))
-                      (str "SELECT DISTINCT " (.-CONCATENATED_FIELDS tl)
-                           " FROM " $
-                           " ORDER BY " order " " direction
-                           " LIMIT 25 OFFSET {{}}")
-                      (s/replace $ #"\{\{\}\}" (create-generator parameter-templates)))}))
+    (clj->js {:values (conj values offset)
+              :text   (as-> text $
+                            (if (empty? $)
+                              "games"
+                              (str "(" $ ") AS GameSubquery NATURAL INNER JOIN games"))
+                            (str "SELECT DISTINCT " (.-CONCATENATED_FIELDS tl)
+                                 " FROM " $
+                                 " ORDER BY " order " " direction
+                                 " LIMIT 25 OFFSET {{}}")
+                            (s/replace $ #"\{\{\}\}" (create-generator parameter-templates)))})))
