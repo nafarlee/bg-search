febe9e3de1cb6686152ed17fcafc14d3bb4c90c7
Nicholas Farley <nicholasafarley@gmail.com>
2021-04-10 13:06:05 -0400

    Add tests for simple query generator

--- src/transpile_test.cljs
+++ src/transpile_test.cljs
@@ -1,5 +1,6 @@
 (ns transpile-test
   (:require
+    ["/transpile/lib" :default tl]
     [clojure.string :as s]
     [transpile :as t]
     [cljs.test :refer [deftest is]]))
@@ -13,3 +14,11 @@
         values (js->clj (.-values actual))]
     (is (= text "SELECT DISTINCT id, primary_name, rating_votes, average_rating, steamdb_rating, bayes_rating, rating_deviation, average_weight, weight_votes, year, minimum_age, minimum_players, maximum_players, minimum_playtime, maximum_playtime, description FROM games ORDER BY id DESC LIMIT 25 OFFSET $1"))
     (is (= values [0]))))
+
+(deftest simple
+  (let [simple-fruit          ((.-__simple tl) "fruit")
+        value                 "pear"
+        {:strs [text values]} (js->clj (simple-fruit #js{:value value}))]
+    (is (= values [(str "%" value "%")]))
+    (is (= (compact-whitespace text)
+           "SELECT id FROM games WHERE fruit ~~* {{}}"))))
