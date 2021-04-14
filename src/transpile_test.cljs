565ab8421e2b58c811421fb0c263594a55614da4
Nicholas Farley <nicholasafarley@gmail.com>
2021-04-14 01:27:48 -0400

    Rewrite transpile test for new interface

--- src/transpile_test.cljs
+++ src/transpile_test.cljs
@@ -9,11 +9,13 @@
   (s/replace s #"\s+" " "))
 
 (deftest transpile
-  (let [actual (t/transpile "" "id" "DESC" 0)
-        text   (s/replace (.-text actual) #"\s+" " ")
-        values (js->clj (.-values actual))]
-    (is (= text "SELECT DISTINCT id, primary_name, rating_votes, average_rating, steamdb_rating, bayes_rating, rating_deviation, average_weight, weight_votes, year, minimum_age, minimum_players, maximum_players, minimum_playtime, maximum_playtime, description FROM games ORDER BY id DESC LIMIT 25 OFFSET $1"))
-    (is (= values [0]))))
+  (is (= (t/transpile "" "id" "DESC" 0)
+         (-> (sql/clj->sql :select :distinct t/exported-fields
+                           :from :games
+                           :order :by :id :desc
+                           :limit :25 :offset #{0})
+             sql/realize-query
+             clj->js))))
 
 (deftest simple
   (is (= (t/simple :fruit {:value "pear"})
