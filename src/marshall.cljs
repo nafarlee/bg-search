38501fd76fa28ce438291b5c8b82e48b0a54c78c
Nicholas Farley <nicholasafarley@gmail.com>
2021-04-20 21:52:11 -0400

    Add incomplete cljs-based marshall fn

---
+++ src/marshall.cljs
@@ -0,0 +1,48 @@
+(ns marshall)
+
+(defn primary-name? [m]
+  (= "primary" (get m "@_type")))
+
+(defn marshall [game]
+  {:api-version
+   3
+
+   :id
+   (get game "@_id")
+
+   :image
+   (get game "image")
+
+   :thumbnail
+   (get game "thumbnail")
+
+   :primary-name
+   (->> (get game "name")
+        (filter primary-name?)
+        (map #(get % "@_value"))
+        first)
+
+   :alternate-names nil
+   :description nil
+   :year nil
+   :minimum-players nil
+   :maximum-players nil
+   :community-recommended-players nil
+   :minimum-playtime nil
+   :maximum-playtime nil
+   :minimum-age nil
+   :rating-votes nil
+   :average-rating nil
+   :bayes-rating nil
+   :rating-deviation nil
+   :weight-votes nil
+   :average-weight nil
+   :categories nil
+   :mechanics nil
+   :families nil
+   :expanded-by nil
+   :contained-in nil
+   :reimplemented-by nil
+   :designers nil
+   :artists nil
+   :publishers nil})
