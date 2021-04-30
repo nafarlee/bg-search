f9791ceb0e6f612747b8c9969de7c9fecf7ad85f
Nicholas Farley <nicholasafarley@gmail.com>
2021-04-30 15:34:48 -0400

    Add language-test ns

---
+++ src/language_test.cljs
@@ -0,0 +1,34 @@
+(ns language-test
+  (:require
+    ["/language/index" :default lang]
+    [cljs.test :refer [deftest is]]))
+
+(deftest parse
+  (let [actual (js->clj (.tryParse lang "-is:e (m:\"Dice Rolling\" bp=2) or (c:economic bp=4)"))
+        expected (js->clj
+                  (clj->js
+                   [{:negate true
+                     :type "META"
+                     :tag "EXPANSION"}
+                    {:type "OR"
+                     :terms [{:type "AND"
+                              :terms [{:negate false
+                                       :type "DECLARATIVE"
+                                       :tag "MECHANIC"
+                                       :value "Dice Rolling"}
+                                      {:negate false
+                                       :type "RELATIONAL"
+                                       :tag "BEST_PLAYERS"
+                                       :operator "="
+                                       :value "2"}]}
+                             {:type "AND"
+                              :terms [{:negate false
+                                       :type "DECLARATIVE"
+                                       :tag "CATEGORY"
+                                       :value "economic"}
+                                      {:negate false
+                                       :type "RELATIONAL"
+                                       :tag "BEST_PLAYERS"
+                                       :operator "="
+                                       :value "4"}]}]}]))]
+    (is (= actual expected))))
