0137489b40e71d2580348f07175b53eec0c6ff8f
Nicholas Farley <nicholasafarley@gmail.com>
2021-05-17 22:44:38 -0400

    Add test for /src/db/insert#toSQL

--- src/sql_test.cljs
+++ src/sql_test.cljs
@@ -1,6 +1,7 @@
 (ns sql-test
   (:require
     [sql]
+    ["/db/insert" :refer [toSQL]]
     [cljs.test :refer [deftest is]]))
 
 (deftest clj->sql
@@ -22,3 +23,12 @@
   (is (= {:text "select id from table where id = $1"
           :values [1]}
          (sql/realize-query (sql/clj->sql :select :id :from :table :where :id := #{1})))))
+
+(deftest ->inserts
+  (is (= (js->clj (toSQL "my-table"
+                         #js["id" "name"]
+                         #js["id"]
+                         (clj->js [[1 "banana"]
+                                   [2 "pear"]])))
+         ["INSERT INTO my-table (id, name)\n     VALUES ($1, $2), ($3, $4)\n     ON CONFLICT (id)\n     DO UPDATE SET id = EXCLUDED.id, name = EXCLUDED.name;"
+          [1 "banana" 2 "pear"]])))
