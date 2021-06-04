4e1af7884559da4f272e3f7b903d45e26f7217d4
Nicholas Farley <nicholasafarley@gmail.com>
2021-06-03 20:09:27 -0400

    Move games fn below "helper" fn

--- src/sql/insert.cljs
+++ src/sql/insert.cljs
@@ -24,29 +24,6 @@
               :on :conflict (list uniques)
               :do :update :set updates)))
 
-(defn games [gs]
-  (let [columns     [:id
-                     :image
-                     :thumbnail
-                     :average_rating
-                     :bayes_rating
-                     :description
-                     :maximum_players
-                     :maximum_playtime
-                     :minimum_age
-                     :minimum_players
-                     :minimum_playtime
-                     :primary_name
-                     :rating_deviation
-                     :rating_votes
-                     :weight_votes
-                     :year]
-        game->chunk (fn [game]
-                      (map #(get game (-> % name string/snake->kebab))
-                           columns))
-        chunks      (map game->chunk (js->clj gs))]
-    (generate :games columns [:id] chunks)))
-
 (defn one-to-many [property game]
   (->> (get game property)
        (map (juxt #(get % "id") #(get % "value")))
@@ -71,6 +48,29 @@
        (map f)
        (filter some?)
        (apply union)))
+
+(defn games [gs]
+  (let [columns     [:id
+                     :image
+                     :thumbnail
+                     :average_rating
+                     :bayes_rating
+                     :description
+                     :maximum_players
+                     :maximum_playtime
+                     :minimum_age
+                     :minimum_players
+                     :minimum_playtime
+                     :primary_name
+                     :rating_deviation
+                     :rating_votes
+                     :weight_votes
+                     :year]
+        game->chunk (fn [game]
+                      (map #(get game (-> % name string/snake->kebab))
+                           columns))
+        chunks      (map game->chunk (js->clj gs))]
+    (generate :games columns [:id] chunks)))
 
 (defn reimplementations [games]
   (let [columns ["original" "reimplementation"]]
