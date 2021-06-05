dde31b688fd3ffd6b228afda53cdfe829ebbe378
Nicholas Farley <nicholasafarley@gmail.com>
2021-06-05 15:26:02 -0400

    Namespace all sql.dsl usage

--- src/sql_test.cljs
+++ src/sql_test.cljs
@@ -1,24 +1,24 @@
 (ns sql-test
   (:require
-    [sql.dsl :refer [clj->sql realize-query]]
+    [sql.dsl :as sd]]
     [cljs.test :refer [deftest is]]))
 
 (deftest clj->sql
   (is (= {:text ["select" "id" "from" "table" "where" "id" "=" :? "or" "id" "=" :?] :values [1 2]}
-         (clj->sql :select :id
+         (sd/clj->sql :select :id
                        :from :table
                        :where :id := #{1}
                           :or :id := #{2})))
   (is (= {:text ["select" "id" "from" "table"] :values []}
-         (clj->sql :select :id
+         (sd/clj->sql :select :id
                        :from :table)))
   (is (= {:text ["select" "id," "name" "from" "table"] :values []}
-         (clj->sql :select [:id :name]
+         (sd/clj->sql :select [:id :name]
                        :from :table)))
   (is (= {:text ["select" "true" "and" "(" "true" "or" "false" ")"] :values []}
-         (clj->sql :select :true :and '(:true :or :false)))))
+         (sd/clj->sql :select :true :and '(:true :or :false)))))
 
 (deftest realize-query
   (is (= {:text "select id from table where id = $1"
           :values [1]}
-         (realize-query (clj->sql :select :id :from :table :where :id := #{1})))))
+         (sd/realize-query (sd/clj->sql :select :id :from :table :where :id := #{1})))))
