45d004dbb09cfb77f86d4866b662a45c9f521c7d
Nicholas Farley <nicholasafarley@gmail.com>
2021-06-08 21:16:30 -0400

    Add promise.js-promise? fn

--- src/promise.cljs
+++ src/promise.cljs
@@ -2,3 +2,9 @@
 
 (defn then-not [p on-failed on-resolved]
   (.then p on-resolved on-failed))
+
+(defn js-promise? [x]
+  {:post [(boolean? %)]}
+  (and (some? x)
+       (= js/Function
+          (type (.-then x)))))
