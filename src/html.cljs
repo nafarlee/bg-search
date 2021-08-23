d00d42426a85857b538f52c14e54c9b8f51eb39c
Nicholas Farley <nicholasafarley@gmail.com>
2021-08-23 19:12:22 -0400

    Update html/clj->html to pass new tests

--- src/html.cljs
+++ src/html.cljs
@@ -1,20 +1,27 @@
 (ns html)
 
-(defn clj->html [[tag attributes & body]]
+(defn render-attributes [attributes]
+  (reduce (fn [acc [k v]]
+            (str acc " " (name k) "=\"" v "\""))
+          ""
+          attributes))
+
+(declare clj->html)
+
+(defn render-element [[tag attributes & body]]
   (if-not (map? attributes)
     (recur (concat [tag {} attributes] body))
-    (str "<"
-         (name tag)
-         (reduce (fn [acc [k v]] (str acc " " (name k) "=\"" v "\""))
-                 ""
-                 attributes)
-         ">"
-         (reduce (fn [acc b]
-                   (if (string? b)
-                     (str acc b)
-                     (str acc (clj->html b))))
-                 ""
-                 body)
-         "</"
-         (name tag)
-         ">")))
+    (str
+     "<"
+     (name tag)
+     (render-attributes attributes)
+     ">"
+     (apply str (map clj->html body))
+     "</"
+     (name tag)
+     ">")))
+
+(defn clj->html [element]
+  (if-not (vector? element)
+    (str element)
+    (render-element element)))
