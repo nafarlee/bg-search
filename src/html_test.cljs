d664b4f14bf29e7946a0c23e215845e47a87034d
Nicholas Farley <nicholasafarley@gmail.com>
2021-08-23 19:10:38 -0400

    Add test for multi-level HTML

--- src/html_test.cljs
+++ src/html_test.cljs
@@ -7,4 +7,10 @@
   (is (= (html/clj->html [:h1 "Hello!"])
          "<h1>Hello!</h1>"))
   (is (= (html/clj->html [:h1 {:class "hidden"} "Hello!"])
-         "<h1 class=\"hidden\">Hello!</h1>")))
+         "<h1 class=\"hidden\">Hello!</h1>"))
+  (is (= "<html><head><title>Hello</title></head><body><h1>World</h1></body></html>"
+         (html/clj->html [:html
+                          [:head
+                           [:title "Hello"]]
+                          [:body
+                           [:h1 "World"]]]))))
