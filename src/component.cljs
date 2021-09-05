9076811bf2ce9d2d35cf694305a2b003d1b83776
Nicholas Farley <nicholasafarley@gmail.com>
2021-09-05 14:21:24 -0400

    Define component/search fn

--- src/component.cljs
+++ src/component.cljs
@@ -23,3 +23,42 @@
   (map (fn [[k v]]
          [:option {:value (name k) :selected (= (name k) selected)} v])
        m))
+
+(defn search [{:keys [query order direction]}]
+  (let [possible-orders {:id "ID"
+                         :primary_name "Name"
+                         :rating_votes "Number of Ratings"
+                         :average_rating "Average Rating"
+                         :steamdb_rating "SteamDB Rating"
+                         :bayes_rating "Geek Rating"
+                         :rating_deviation "Rating Deviation"
+                         :average_weight "Weight"
+                         :weight_votes "Number of Weight Ratings"
+                         :year "Release Year"
+                         :age "Minimum Age"
+                         :minimum_players "Minimum Players"
+                         :maximum_players "Maximum Players"
+                         :minimum_playtime "Minimum Playtime"
+                         :maximum_playtime "Maximum Playtime"}
+        possible-directions {:DESC "Descending"
+                             :ASC "Ascending"}
+        search-attributes {:spellcheck "false"
+                           :autocomplete "off"
+                           :autocapitalize "off"
+                           :autocorrect "off"
+                           :value query
+                           :type "search"
+                           :mozactionhint "search"
+                           :name "query"}]
+    [:div {:class "row"}
+     [:div {:class "column"}
+      [:input search-attributes]]]
+    [:div {:class "row"}
+     [:div {:class "column"}
+      [:label {:for "order"} "Sort"]
+      [:select {:name "order"}
+       (options order possible-orders)]]
+     [:div {:class "column"}
+      [:label {:for "direction"} "Direction"]
+      [:select {:name "direction"}
+       (options direction possible-directions)]]]))
