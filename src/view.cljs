82e33b23940dd6c05148cfdaa2302ebc7cf5e714
Nicholas Farley <nicholasafarley@gmail.com>
2021-09-05 13:40:48 -0400

    Rename view/head -> component/head

--- src/view.cljs
+++ src/view.cljs
@@ -1,5 +1,6 @@
 (ns view
   (:require
+    [component :refer [head]]
     [clojure.string :as s]
     [html :refer [html doctype]]))
 
@@ -21,25 +22,6 @@
 (defn- sort-recommendations [recommendations]
   (sort-by #(-> % (get "players") range->text (js/parseInt 10))
            recommendations))
-
-(def head
-  (list
-   [:link
-    {:rel "stylesheet"
-     :href "https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic"}]
-   [:link
-    {:rel "stylesheet"
-     :href "https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"}]
-   [:link
-    {:rel "stylesheet"
-     :href "https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.css"}]
-   [:link
-    {:rel "stylesheet"
-     :type "text/css"
-     :href "/style.css"}]
-   [:meta
-    {:name "viewport"
-     :content "width=device-width, initial-scale=1"}]))
 
 (defn options [selected m]
   (map (fn [[k v]]
