0cc3453dfef3b6610c27ae127fb3707234d90795
Nicholas Farley <nicholasafarley@gmail.com>
2021-09-05 14:43:42 -0400

    Replace inline search component with component/search

--- src/view.cljs
+++ src/view.cljs
@@ -1,6 +1,6 @@
 (ns view
   (:require
-    [component :refer [head options]]
+    [component :refer [head options search]]
     [clojure.string :as s]
     [html :refer [html doctype]]))
 
@@ -24,32 +24,7 @@
            recommendations))
 
 (defn search [{:keys [games query next-url direction order]}]
-  (let [possible-orders {:id "ID"
-                         :primary_name "Name"
-                         :rating_votes "Number of Ratings"
-                         :average_rating "Average Rating"
-                         :steamdb_rating "SteamDB Rating"
-                         :bayes_rating "Geek Rating"
-                         :rating_deviation "Rating Deviation"
-                         :average_weight "Weight"
-                         :weight_votes "Number of Weight Ratings"
-                         :year "Release Year"
-                         :age "Minimum Age"
-                         :minimum_players "Minimum Players"
-                         :maximum_players "Maximum Players"
-                         :minimum_playtime "Minimum Playtime"
-                         :maximum_playtime "Maximum Playtime"}
-        possible-directions {:DESC "Descending"
-                             :ASC "Ascending"}
-        search-attributes {:spellcheck "false"
-                           :autocomplete "off"
-                           :autocapitalize "off"
-                           :autocorrect "off"
-                           :value query
-                           :type "search"
-                           :mozactionhint "search"
-                           :name "query"}
-        game->heading (fn [{:strs [id year primary_name]}]
+  (let [game->heading (fn [{:strs [id year primary_name]}]
                         [:p [:a {:href (str "/games/" id)}
                              (str primary_name " (" year ")")]])]
     (html
@@ -58,19 +33,7 @@
       [:html
        [:head head]
        [:body {:class "container"}
-        [:form {:method "get" :action "/search"}
-         [:div {:class "row"}
-          [:div {:class "column"}
-           [:input search-attributes]]]
-         [:div {:class "row"}
-          [:div {:class "column"}
-           [:label {:for "order"} "Sort"]
-           [:select {:name "order"}
-            (options order possible-orders)]]
-          [:div {:class "column"}
-           [:label {:for "direction"} "Direction"]
-           [:select {:name "direction"}
-            (options direction possible-directions)]]]]
+        (search {:query query :order order :direction direction})
         (if (empty? games)
           [:h1 {:class "center"} "No more results!"]
           (list
