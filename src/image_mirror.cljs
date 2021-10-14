e98a07892e542e5978206b9014b5d957712dab65
Nicholas Farley <nicholasafarley@gmail.com>
2021-10-13 21:17:51 -0400

    Add function to create a file download stream

--- src/image_mirror.cljs
+++ src/image_mirror.cljs
@@ -1,6 +1,17 @@
 (ns image-mirror
   (:require
+    ["https" :as https]
     ["util" :as util]
     ["stream" :as stream]))
 
 (def pipeline (util/promisify stream/pipeline))
+
+(defn download-stream [url]
+  (js/Promise.
+   (fn [fulfill reject]
+     (https/get
+      url
+      (fn [res]
+        (if (== 200 (.-statusCode res))
+          (fulfill res)
+          (reject res)))))))
