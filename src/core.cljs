f9c9ebb91705f24cb57e33681bc977ce7f8d1a28
Nicholas Farley <nicholasafarley@gmail.com>
2021-10-20 16:42:15 -0400

    Switch from with-database to with-database-pool for all routes

--- src/core.cljs
+++ src/core.cljs
@@ -1,43 +1,45 @@
 (ns core
   (:require
+    [sql :as sql]
     ["express" :as express]
     [routes :as routes]
-    [middleware :refer [with-database with-header with-required-body-parameters]]))
+    [middleware :refer [with-database-pool with-header with-required-body-parameters]]))
 
 (defonce ^:export app (express))
 
 (defn main []
-  (doto app
-        (.use (.urlencoded express #js{:extended true}))
-        (.get
-         "/"
-         (-> routes/index
-             (with-header "Cache-Control" (str "public, max-age=" (* 60 60 24)))))
-        (.use (.static express "public"))
-        (.get
-         "/image-mirror/:url(\\S+)"
-         routes/image-mirror)
-        (.get
-         "/search"
-         (-> routes/search
-             (with-header "Cache-Control" (str "public, max-age=" (* 60 60 24 7)))
-             with-database))
-        (.post
-         "/pubsub/pull"
-         (-> routes/pull
-             with-database))
-        (.post
-         "/pubsub/pull-plays"
-         (-> routes/pull-plays
-             with-database))
-        (.post
-         "/pull-collection"
-         (-> routes/pull-collection
-             with-database
-             (with-required-body-parameters #{"username"})))
-        (.get
-         "/games/:id"
-         (-> routes/games
-             (with-header "Cache-Control" (str "public, max-age=" (* 60 60 24 7)))
-             with-database))
-        (.listen 8080 #(prn "Listening on 8080..."))))
+  (let [pool (delay (sql/pool))]
+    (doto app
+          (.use (.urlencoded express #js{:extended true}))
+          (.get
+           "/"
+           (-> routes/index
+               (with-header "Cache-Control" (str "public, max-age=" (* 60 60 24)))))
+          (.use (.static express "public"))
+          (.get
+           "/image-mirror/:url(\\S+)"
+           routes/image-mirror)
+          (.get
+           "/search"
+           (with-database-pool pool)
+           (-> routes/search
+               (with-header "Cache-Control" (str "public, max-age=" (* 60 60 24 7)))))
+          (.post
+           "/pubsub/pull"
+           (with-database-pool pool)
+           routes/pull)
+          (.post
+           "/pubsub/pull-plays"
+           (with-database-pool pool)
+           routes/pull-plays)
+          (.post
+           "/pull-collection"
+           (with-database-pool pool)
+           (-> routes/pull-collection
+               (with-required-body-parameters #{"username"})))
+          (.get
+           "/games/:id"
+           (with-database-pool pool)
+           (-> routes/games
+               (with-header "Cache-Control" (str "public, max-age=" (* 60 60 24 7)))))
+          (.listen 8080 #(prn "Listening on 8080...")))))
