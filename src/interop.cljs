dfcbc2c2ab59932dc140f5c3a2cc9e42292e3ee0
Nicholas Farley <nicholasafarley@gmail.com>
2021-11-06 20:13:43 -0400

    Add interop/js-error fn

--- src/interop.cljs
+++ src/interop.cljs
@@ -3,3 +3,7 @@
 (defn parse-int
   ([s b] (js/parseInt s b))
   ([s] (parse-int s 10)))
+
+(defn js-error
+  ([message] (js/Error. message))
+  ([message cause] (js/Error. message #js{:cause cause})))
