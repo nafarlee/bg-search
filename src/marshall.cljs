2a97108b031fd4e0662064180c46f90592f2ff00
Nicholas Farley <nicholasafarley@gmail.com>
2021-12-26 16:45:26 -0500

    Create map for language dependency slugs

--- src/marshall.cljs
+++ src/marshall.cljs
@@ -23,6 +23,13 @@
   {:id    (-> x get-id (js/parseInt 10))
    :value (get-value x)})
 
+(def ^:private language-dependencies
+  {"No necessary in-game text"                                        0
+   "Some necessary text - easily memorized or small crib sheet"       1
+   "Moderate in-game text - needs crib sheet or paste ups"            2
+   "Extensive use of text - massive conversion needed to be playable" 3
+   "Unplayable in another language"                                   4})
+
 (defn- marshall-game [game]
   {:api-version
    3
