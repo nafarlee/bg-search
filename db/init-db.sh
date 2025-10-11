0be5800afaa20100b784af5a269d3987ee38691f
Nicholas Farley <nicholasafarley@gmail.com>
2025-10-11 18:22:44 -0400

    Remove unused db/ files

--- db/init-db.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env oil
-docker run --network host \
-           -it \
-           -v "$PWD":/_ \
-           postgres:11.3 \
-           sh -c "psql -h localhost -U postgres -f _/db.sql"
