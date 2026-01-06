126861167e45d7b4369af4237845555456d65b2d
Nicholas Farley <nicholasafarley@gmail.com>
2026-01-05 22:05:07 -0500

    Add draft of pull-games daemon (with memory leak)

--- src/pull_games.cljs
+++ src/pull_games.cljs
@@ -1,4 +1,50 @@
-(ns pull-games)
+(ns pull-games
+ (:require
+  [api :refer [get-games]]
+  [util :refer [with-retry]]
+  [sql.insert :refer [insert]]
+  [sql :refer [get-game-checkpoint
+               get-game-id-cliff
+               insert-games
+               mobius-games
+               pool]]))
 
 (defn main []
-  (println "Hello, World!"))
+  (loop [db (pool)
+         p  (js/Promise.resolve nil)]
+    (let [checkpoint-p
+          (.then p
+                 (fn [_]
+                   (with-retry #(get-game-checkpoint db) 3)))
+
+          new-checkpoint-p
+          (.then checkpoint-p
+                 #(+ % 20))
+
+          cliff-p
+          (.then p
+                 (fn [_]
+                   (with-retry #(get-game-id-cliff db) 3)))
+
+          games-p
+          (.then (js/Promise.all #js[checkpoint-p new-checkpoint-p])
+                 (fn [[checkpoint new-checkpoint]]
+                   (get-games (range checkpoint new-checkpoint))))
+          
+          insertions-p
+          (.then games-p #(insert %))
+
+          did-insert-p?
+          (.then (js/Promise.all #js[insertions-p new-checkpoint-p])
+                 (fn [[insertions new-checkpoint]]
+                   (insert-games db insertions new-checkpoint)))
+
+          mobius-p
+          (.then (js/Promise.all #js[did-insert-p? new-checkpoint-p cliff-p])
+                 (fn [[_ new-checkpoint cliff]]
+                   (when (< cliff new-checkpoint)
+                     (mobius-games db))))
+          
+          result-p
+          (.catch mobius-p js/console.error)]
+      (recur db result-p))))
