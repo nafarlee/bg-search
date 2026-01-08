2e89e75db25ec000f127132019da17b1c703fe79
Nicholas Farley <nicholasafarley@gmail.com>
2026-01-08 00:29:48 -0500

    Make pull-games daemon 'properly' tail recursive

--- src/pull_games.cljs
+++ src/pull_games.cljs
@@ -9,42 +9,41 @@
                mobius-games
                pool]]))
 
+(defn pull-games [db]
+  (let [checkpoint-p
+        (with-retry #(get-game-checkpoint db) 3)
+
+        new-checkpoint-p
+        (.then checkpoint-p
+               #(+ % 20))
+
+        cliff-p
+        (with-retry #(get-game-id-cliff db) 3)
+
+        games-p
+        (.then (js/Promise.all #js[checkpoint-p new-checkpoint-p])
+               (fn [[checkpoint new-checkpoint]]
+                 (get-games (range checkpoint new-checkpoint))))
+        
+        insertions-p
+        (.then games-p #(insert %))
+
+        did-insert-p?
+        (.then (js/Promise.all #js[insertions-p new-checkpoint-p])
+               (fn [[insertions new-checkpoint]]
+                 (insert-games db insertions new-checkpoint)))
+
+        mobius-p
+        (.then (js/Promise.all #js[did-insert-p? new-checkpoint-p cliff-p])
+               (fn [[_ new-checkpoint cliff]]
+                 (when (< cliff new-checkpoint)
+                   (mobius-games db))))]
+    (-> mobius-p
+        (.then (fn [_]
+                 (pull-games db)
+                 nil))
+        (.catch js/console.error))))
+  
+
 (defn main []
-  (loop [db (pool)
-         p  (js/Promise.resolve nil)]
-    (let [checkpoint-p
-          (.then p
-                 (fn [_]
-                   (with-retry #(get-game-checkpoint db) 3)))
-
-          new-checkpoint-p
-          (.then checkpoint-p
-                 #(+ % 20))
-
-          cliff-p
-          (.then p
-                 (fn [_]
-                   (with-retry #(get-game-id-cliff db) 3)))
-
-          games-p
-          (.then (js/Promise.all #js[checkpoint-p new-checkpoint-p])
-                 (fn [[checkpoint new-checkpoint]]
-                   (get-games (range checkpoint new-checkpoint))))
-          
-          insertions-p
-          (.then games-p #(insert %))
-
-          did-insert-p?
-          (.then (js/Promise.all #js[insertions-p new-checkpoint-p])
-                 (fn [[insertions new-checkpoint]]
-                   (insert-games db insertions new-checkpoint)))
-
-          mobius-p
-          (.then (js/Promise.all #js[did-insert-p? new-checkpoint-p cliff-p])
-                 (fn [[_ new-checkpoint cliff]]
-                   (when (< cliff new-checkpoint)
-                     (mobius-games db))))
-          
-          result-p
-          (.catch mobius-p js/console.error)]
-      (recur db result-p))))
+  (pull-games (pool)))
