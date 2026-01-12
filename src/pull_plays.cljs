4be1c8a4f92148d9f0b76d3075d35581bef2fb50
Nicholas Farley <nicholasafarley@gmail.com>
2026-01-11 23:11:34 -0500

    Create first draft of pull-plays daemon

--- src/pull_plays.cljs
+++ src/pull_plays.cljs
@@ -2,15 +2,61 @@
  (:require
   [cljs.core.async :refer [go-loop]]
   [cljs.core.async.interop :refer-macros [<p!]]
-  [util :refer [with-retry]]
-  [http :refer [sleep]]
-  [sql :refer [pool]]))
+  [api :refer [get-plays]]
+  [sql :refer [game? 
+               get-last-game
+               get-plays-checkpoint
+               play?
+               pool
+               save-plays
+               update-plays-checkpoint]]
+  [util :refer [with-retry]]))
+
+(defn positive-play? [[_ _ play-time]]
+  (pos? play-time))
 
 (defn pull-plays [db]
-  (go-loop []
-    (js/console.log (<p! (.query db "SELECT 42")))
-    (<p! (sleep 5000))
-    (recur)))
+  (go-loop [[game-id page] (<p! (get-plays-checkpoint db))]
+    (cond
+      (< (<p! (get-last-game db)) game-id)
+      (do
+        (<p! (update-plays-checkpoint db 1 1))
+        (prn :surpassed-game-id {:game-id game-id})
+        (recur [1 1]))
+
+      (not (<p! (game? db game-id)))
+      (do
+        (<p! (update-plays-checkpoint db (inc game-id) 1))
+        (prn :not-a-game {:game-id game-id})
+        (recur [(inc game-id) 1]))
+
+      :else
+      (let [plays (<p! (get-plays game-id page))
+            positive-plays (filter positive-play? plays)]
+        (cond
+          (empty? plays)
+          (do
+            (<p! (update-plays-checkpoint db (inc game-id) 1))
+            (prn :no-plays {:game-id game-id :page page})
+            (recur [(inc game-id) 1]))
+
+          (empty? positive-plays)
+          (do
+            (<p! (update-plays-checkpoint db game-id (inc page)))
+            (prn :no-positive-plays {:game-id game-id :page page})
+            (recur [game-id (inc page)]))
+
+          (<p! (play? db (ffirst positive-plays)))
+          (do
+            (<p! (update-plays-checkpoint db (inc game-id) 1))
+            (prn :no-new-plays {:game-id game-id :page page})
+            (recur [(inc game-id) 1]))
+
+          :else
+          (do
+            (<p! (save-plays db game-id page positive-plays))
+            (prn :save-plays {:game-id game-id :page page})
+            (recur [game-id (inc page)])))))))
 
 (defn main []
   (let [db (pool)]
