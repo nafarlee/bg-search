300e0687727b0b3de5805b627780a96eea6a1dc2
Nicholas Farley <nicholasafarley@gmail.com>
2026-02-05 23:24:37 -0500

    Allow passing a toast message as a query parameter

--- src/core.cljs
+++ src/core.cljs
@@ -17,8 +17,11 @@
           (.get
            "/"
            (m/with-header "Cache-Control" (str "public, max-age=" (* 60 60 24)))
-           (fn [_req res]
-             (.send res (v/index))))
+           m/with-query-params
+           (fn [^js req res]
+             (let [{:keys [query]} (.-locals req)
+                   {:keys [toast]} query]
+               (.send res (v/index :toast toast)))))
 
           (.use (.static express "public"))
 
